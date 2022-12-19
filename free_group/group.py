4234f43b96e7dfb73cd7f5f2732680bdcb0cae80
luxfabian <fabian.lux@yu.edu>
2022-12-19 17:09:50 -0500

    PEP8

--- free_group/group.py
+++ free_group/group.py
@@ -11,21 +11,22 @@ import numpy as np
 
 
 # -- basic group elements
-e = np.array([[1,0],[0,1]], dtype=int)
+e = np.array([[1, 0], [0, 1]], dtype=int)
 a = np.array([[1, 2], [0, 1]], dtype=int)
 b = np.array([[1, 0], [2, 1]], dtype=int)
 A = np.array([[1, -2], [0, 1]], dtype=int)
 B = np.array([[1, 0], [-2, 1]], dtype=int)
 
 # generators =  np.array( [a,b,A,B] , dtype=int)
-generators =  np.array( [a,b] , dtype=int)
+generators = np.array([a, b], dtype=int)
 
 
-def mult(h,g,N):
+def mult(h, g, N):
     """
         Matrix multiplication modulo N
     """
-    return np.dot(h,g) % N
+    return np.dot(h, g) % N
+
 
 def encode(x):
     """
