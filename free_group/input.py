a7b60ce12618a1c26e999c21e3d73f3c8fb7b895
luxfabian <fabian.lux@yu.edu>
2022-12-19 17:21:55 -0500

    Input

---
+++ free_group/input.py
@@ -0,0 +1,12 @@
+"""
+    ./free_group/input.py
+
+    Author: Fabian R. Lux
+    Date:   12/19/2022
+    Mail:   fabian.lux@yu.edu
+
+    Input file which specifies which finite approximation should be computed.
+    The approximations come about by modding w.r.t p**N
+"""
+p = 2
+N = 3
